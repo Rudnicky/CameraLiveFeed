61a6f500d896f1cd4061d6ca6f81bfc92ee14b7e
Pawel Rudnicki <p.rudnicki@insys.pl>
2020-06-06 23:03:33 +0200

    Added login & passwords commands

--- CameraLiveFeed/ViewModels/LoginViewModel.cs
+++ CameraLiveFeed/ViewModels/LoginViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using CameraLiveFeed.Core.Services.RelayCommand;
 using CameraLiveFeed.ViewModels.Base;
-using System;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -15,12 +14,32 @@ namespace CameraLiveFeed.ViewModels
             set => SetProperty(ref _isLoginButtonEnabled, value);
         }
 
-        public ICommand LoginButtonClickedCommand { get => new AsyncCommand(LoginButtonClickedAsync, CanExecute); }
-        public ICommand WatermarkTextChangedCommand { get => new AsyncCommand<object>(WatermarkTextChanged, CanExecute); }
-
-        private async Task WatermarkTextChanged(object arg)
+        private string _username;
+        public string Username
         {
+            get => _username;
+            set => SetProperty(ref _username, value);
+        }
 
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public ICommand LoginButtonClickedCommand { get => new AsyncCommand(LoginButtonClickedAsync, CanExecute); }
+        public ICommand WatermarkTextChangedCommand { get => new RelayCommand(WatermarkTextChanged, CanExecute); }
+        public ICommand PasswordChangedCommand { get => new RelayCommand(PasswordChanged, CanExecute); }
+
+        private void WatermarkTextChanged()
+        {
+            IsLoginButtonEnabled = !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
+        }
+
+        private void PasswordChanged()
+        {
+            IsLoginButtonEnabled = !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
         }
 
         private async Task LoginButtonClickedAsync()
